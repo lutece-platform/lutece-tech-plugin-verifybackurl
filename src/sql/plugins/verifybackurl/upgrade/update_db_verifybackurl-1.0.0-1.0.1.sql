31c0149a5dd2881dfec8483264d9efdf4499ec09
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-12-02 11:16:12 +0100

    LUT-31351 : Add liquibase tags to SQL scripts and remove empty SQL scripts

--- src/sql/plugins/verifybackurl/upgrade/update_db_verifybackurl-1.0.0-1.0.1.sql
+++ src/sql/plugins/verifybackurl/upgrade/update_db_verifybackurl-1.0.0-1.0.1.sql
@@ -1,2 +1,5 @@
+-- liquibase formatted sql
+-- changeset core:update_db_verifybackurl-1.0.0-1.0.1.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE verifybackurl_authorized_url ADD COLUMN application_code varchar(100) DEFAULT NULL;
 
